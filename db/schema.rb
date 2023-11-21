085cf965434887ca0c76b9ab59f3c14631b001c7
n-haruhi <hr23nakanishi@gmail.com>
2023-11-21 14:39:45 +0000

    [Update] 部分テンプレート2

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2023_11_20_081657) do
     t.text "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
